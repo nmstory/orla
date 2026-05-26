18acbf2dcfb62f3402cc0e7ec4f1b33a0147997c
Nathan <45177569+nmstory@users.noreply.github.com>
2026-05-26 18:09:42 +0100

    metrics_registry split

--- include/metrics_registry.h
+++ include/metrics_registry.h
@@ -9,11 +9,7 @@ namespace orla {
 
 class MetricsRegistry {
   public:
-    explicit MetricsRegistry(uint16_t port)
-        : m_Exposer("0.0.0.0:" + std::to_string(port)),
-          m_Registry(std::make_shared<prometheus::Registry>()) {
-        m_Exposer.RegisterCollectable(m_Registry);
-    }
+    explicit MetricsRegistry(uint16_t port);
 
     prometheus::Registry& registry() { return *m_Registry; }
 
